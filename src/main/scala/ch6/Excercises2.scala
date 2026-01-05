3e3c9123a1cc227b2898525963bde4cf8e067429
Paritosh <pk14Irvine@gmail.com>
2026-01-04 19:21:49 -0800

    recursive sequence

--- src/main/scala/ch6/Excercises2.scala
+++ src/main/scala/ch6/Excercises2.scala
@@ -22,6 +22,15 @@ object Excercises2 {
       val (nxtVal, nxtState) = action(rng)
       (f(nxtVal), nxtState)
 
+  def map2[A, B, C](actionOne: Rand[A])(actionTwo: Rand[B])(f: (A,B) => C): Rand[C] =
+    rng =>
+      val (aVal, nxtState) = actionOne(rng)
+      val (bVal, nxtState2) = actionTwo(nxtState)
+      (f(aVal, bVal), nxtState2)
+
+  def both[A, B](actionOne: Rand[A])(actionTwo: Rand[B]): Rand[(A, B)] =
+    map2(actionOne)(actionTwo)((a, b) => (a, b))
+
   val int: Rand[Int] = rng => rng.nextInt
 
   val nonNegativeInt: Rand[Int] =
@@ -55,5 +64,13 @@ object Excercises2 {
     val (numInt, nxtState) = nxtRng.nextInt
     (numInt :: intList, nxtState)
   }
-
+  
+  def sequence[A](rs: List[Rand[A]]): Rand[List[A]] =
+    def recur(rng: RNG, rs: List[Rand[A]], accum: List[A]): (List[A], RNG) =
+      rs match
+        case head :: tail =>
+          val (a, state) = head(rng)
+          recur(state, tail, a :: accum)
+        case _ => (accum, rng)
+    rng => recur(rng, rs, List.empty[A])
 }
